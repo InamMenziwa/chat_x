137a6c8de2a2e427caee33c5e3d8dc07de1538fb
Ijax claw <inammenziwa074@gmail.com>
2024-04-26 16:39:58 +0200

    created first room

--- chatapp/models.py
+++ chatapp/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.name
     
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import ChatRoom
 
 def index(request):
-    return render(request, "chatapp/index.html")
+    rooms = ChatRoom.objects.all()
+    return render(request, "chatapp/index.html", {"rooms":rooms})
