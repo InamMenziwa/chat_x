430679fa4d4edc4625bb701cdb38193d166eae5e
Ijax claw <inammenziwa074@gmail.com>
2024-05-06 21:12:15 +0200

    got the consumer client messaging working

--- chatapp/consumer.py
+++ chatapp/consumer.py
@@ -1,5 +1,6 @@
 from channels.layers import get_channel_layer
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
  
 class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
@@ -12,13 +13,39 @@ class ChatConsumer(AsyncWebsocketConsumer):
  
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.channel_name
+            self.channel_name,
         )
  
         await self.accept()
  
-    async def disconnect(self, close_code):
+    async def disconnect(self):
         await self.channel_layer.group_discard(
             self.room_group_name,
-            self.channel_name
+            self.channel_name,
         )
+        
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        messsage = data["message"]
+        user = data["user_name"]
+        room = data["room"]
+        
+        await self.channel_layer.group_send(
+            self.room_group_name, {
+                "type":"chat_message",
+                "message":messsage,
+                "user":user,
+                "room":room,
+            }
+        )
+        
+    async def chat_message(self, event):
+        messsage = event["message"]
+        user_name = event["user"]
+        room = event["room"]
+        
+        await self.send(text_data=json.dumps({
+            "message":messsage,
+            "user_name":user_name,
+            "room":room,
+        }))
