2f3ad1ad94932070e0cb7e16049e114d2e4d1cf9
Ijax claw <inammenziwa074@gmail.com>
2024-05-10 16:28:19 +0200

    finished product 1

--- chatapp/consumer.py
+++ chatapp/consumer.py
@@ -1,6 +1,9 @@
 from channels.layers import get_channel_layer
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+from .models import messages, ChatRoom
+from django.contrib.auth.models import User
+from asgiref.sync import sync_to_async
  
 class ChatConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
@@ -18,7 +21,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
  
         await self.accept()
  
-    async def disconnect(self):
+    async def disconnect(self, code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
@@ -26,18 +29,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
         
     async def receive(self, text_data):
         data = json.loads(text_data)
-        messsage = data["message"]
+        message = data["message"]
         user = data["user_name"]
         room = data["room"]
         
         await self.channel_layer.group_send(
             self.room_group_name, {
                 "type":"chat_message",
-                "message":messsage,
+                "message":message,
                 "user":user,
                 "room":room,
             }
         )
+        await self.save_message(user,room, message)
         
     async def chat_message(self, event):
         messsage = event["message"]
@@ -49,3 +53,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             "user_name":user_name,
             "room":room,
         }))
+        
+    @sync_to_async
+    def save_message(self, user, room, message):
+        user = User.objects.get(username=user)
+        room = ChatRoom.objects.get(slug=room)
+        messages.objects.create(user=user, room=room, message_content=message)
+        
--- chatapp/views.py
+++ chatapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import ChatRoom
+from .models import ChatRoom, messages
+
 
 def index(request):
     rooms = ChatRoom.objects.all()
@@ -7,4 +8,5 @@ def index(request):
 
 def chatroom(request, slug):
     chatroom = ChatRoom.objects.get(slug=slug)
-    return render(request, "chatapp/room.html", {"chatroom":chatroom})
+    message = messages.objects.filter(room=chatroom)[0:30]
+    return render(request, "chatapp/room.html", {"chatroom":chatroom, "messages":message})
